1e525417c0c6577f56f115141a30d4d2dafd457a
EetuK <eetukoskelainen@hotmail.com>
2020-09-19 12:44:27 +0300

    Add env varibles to deploy

--- add-pulumi-secrets.sh
+++
@@ -1,5 +0,0 @@
-pulumi config set --secret dbPassword S3cr37
-pulumi config set --secret dbPassword S3cr37
-pulumi config set --secret dbPassword S3cr37
-pulumi config set --secret dbPassword S3cr37
-pulumi config set --secret dbPassword S3cr37
